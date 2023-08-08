4c908cc0c437c723bff2438b8a2c5b6b7915ad16
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-08 12:44:33 -0700

    Move server files in server dir

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello there!')
+})
+
+app.listen(3000, () => console.log('Server is running on port 3000'))
